4c0b6648107ad16fb2d0372e019868aa52755cd2
Progress <star@progress.sh>
2022-11-11 06:12:30 +0100

    changed about page

--- pages/about.js
+++ pages/about.js
@@ -31,19 +31,53 @@ export default function About() {
             </h1>
             <main>
               <article className="text-gray-300 font-opensans space-y-3 pt-8 leading-normal text-[0.87rem]">
+                <p>Progress is my pursuit at living life to its fullest.</p>
+
                 <p>
-                  Progress is my attempt at getting better at the things that
-                  are meaningful to me and to strive for life.
+                  I want to reach a higher place, to find happiness, meaning and
+                  make great things.
                 </p>
+
                 <p>
-                  This is my heartfelt desire to go as far as I can, to find my
-                  place in the world. To shine as brightly as I can.
+                  With such grand goals it is easy to look to an abstract
+                  future, wishfully thinking it will be realized.
+                  <br />
+                  However, &quot;one day&quot; does not exist, the future is
+                  only materialized by your actions.
+                  <br />
+                  At this moment, this time, you hold all the keys. The littest
+                  of your steps will pave the way for the next ones.
                 </p>
+
                 <p>
-                  There&apos;s so much in store for the future and I&apos;ll
-                  work hard to see mine come into form with everything I&apos;ve
-                  got.
+                  Don&apos; let go. <br />
+                  Don&apos;t forget.
+                  <br />
+                  Walk towards the future.
                 </p>
+
+                <p>
+                  Find out,
+                  <br />
+                  All the beautiful things waiting there,
+                  <br />
+                  All the painful things that&apos;ll gaze back upon you.
+                  <br />
+                  Who you are now is only a small subset of all the people
+                  you&apos;ll ever be.
+                  <br />
+                  Live as you are while you&apos;re here.
+                </p>
+
+                <p>
+                  I&apos;ll show you everything, failures and success, every
+                  part of the struggle.
+                  <br />
+                  I&apos;ll walk towards that star, symbol of all my
+                  aspirations.
+                </p>
+
+                <p>You are infinite, you can go as far as you&rsquo;d like.</p>
               </article>
             </main>
           </div>
